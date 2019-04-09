8de7c5df206fcae7b6ac4af99d8209ab38a9bfb0
sema0710 <pandati@naver.com>
2019-04-09 16:07:10 +0900

    codeup_1402
    codeup_1402

---
+++ codeup_1403.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(){
+	int i,j;
+	scanf("%d",&i);
+	int num[i];
+	for(j=0;j<i;j++){
+		scanf("%d",&num[j]);
+	}
+	for(j=0;j<i;j++){
+		printf("%d\n",num[j]);
+	}
+	for(j=0;j<i;j++){
+		printf("%d\n",num[j]);
+	}
+}
