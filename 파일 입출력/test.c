5bf7ec678449d68359db99582955555ed22f79f7
sema0710 <pandati@naver.com>
2019-06-27 23:39:33 +0900

    file_input_output_2

--- 파일 입출력/test.c
+++ 파일 입출력/test.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 int main(){
-	FILE *fp1 = fopen("memo1.txt","w");
+	FILE *fp1 = fopen("memo1.txt","r");
 	FILE *fp2 = fopen("memo2.txt","w");
-	fprintf(fp1,"%d",1);
 	int a;
 	fscanf(fp1,"%d",&a);
 	fprintf(fp2,"%d",a);
